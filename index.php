6630c89f0087afb89c596ed08a63aa7e9cc08100
Del Puerto <bdelpuerto@dxc.com>
2019-08-15 23:20:24 +0800

    index

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <?php
-$host = 'containerid';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$host = '7962b6d240e3'; // <<<<<<<<<< replace IP if needed
+$db   = 'dxc';
 $user = 'user';
-$password = 'pass';
-$db = 'dxc';
+$pass = 'pass';
+$charset = 'utf8mb4';
 
-$conn = new mysqli($host,$user,$password,$db);
-
-if($conn->connect_error){
-    echo 'connection failed' . $conn->connect_error;
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$cover = "";
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     //throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    $cover = $e->getMessage(); 
 }
-
-echo 'Successfully connected to MYSQL';
 ?>
