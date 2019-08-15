17d008af2e9bfaa415c2f4cb254d204575a21c50
Del Puerto <bdelpuerto@dxc.com>
2019-08-16 00:02:12 +0800

    add new

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
 <?php
-$servername = "7962b6d240e3";
-$username = "user";
-$password = "pass";
-$dbname = "dxc";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+$host = '7962b6d240e3';
+$db   = 'dxc';
+$user = 'user';
+$pass = 'pass';
+$charset = 'utf8mb4';
 
-$sql = "INSERT INTO users(firstname,lastname) VALUES('Denise','Del');";
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+try {
+    $conn=new PDO($dsn,$user,$pass);
+} catch (PDOException $e) {
+    $cover = $e->getMessage(); 
+}
+
+if($conn->connect_error){
+    echo 'connection failed' . $conn->connect_error;
+}
+else echo 'Successfully connected to MYSQL';
+
+$sql = "INSERT INTO users(firstname,lastname) VALUES('Bianca2','Denise2')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
-}
 
-$conn->close();
 ?>
